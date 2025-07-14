6d025fc73e3ea333e816dace67e88d4802e48798
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-07-14 11:42:20 +0530

    Update user.model.js

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,3 +1,50 @@
+// // models/User.js
+// import mongoose from 'mongoose';
+// import bcrypt from 'bcrypt';
+
+// const { Schema, model } = mongoose;
+
+// const userSchema = new Schema({
+//   fullName: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// }, { timestamps: true });
+
+// // Pre-save middleware to hash password
+// userSchema.pre('save', async function(next) {
+//   if (!this.isModified('password')) {
+//     next();
+//   }
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// // Method to compare passwords
+// userSchema.methods.comparePassword = function(candidatePassword) {
+//   return bcrypt.compare(candidatePassword, this.password);
+// };
+
+// const User = model('User', userSchema);
+
+// export default User;
+
+
+
 // models/User.js
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
@@ -8,6 +55,13 @@ const userSchema = new Schema({
   fullName: {
     type: String,
     required: true
+  },
+  niatId: {
+    type: String,
+    required: [true, "NIAT ID is required"],
+    unique: true,
+    uppercase: true,
+    trim: true,
   },
   email: {
     type: String,
