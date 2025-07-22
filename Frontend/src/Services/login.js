8e5897fc2e0655f8d142a5d6eb19633efd89e9aa
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-07-22 12:20:51 +0530

    Update login.js

--- Frontend/src/Services/login.js
+++ Frontend/src/Services/login.js
@@ -1,3 +1,66 @@
+// import axios from "axios";
+// import { VITE_APP_URL } from "@/config/config";
+
+// const axiosInstance = axios.create({
+//   // **FIX:** Added a slash before "api/"
+//   baseURL: VITE_APP_URL + "/api/",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: true,
+// });
+
+// // ... rest of the file remains the same ...
+
+// const startUser = async () => {
+//   try {
+//     const response = await axiosInstance.get("users/");
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(
+//       error?.response?.data?.message || error?.message || "Something Went Wrong"
+//     );
+//   }
+// };
+
+// const registerUser = async (data) => {
+//   try {
+//     const response = await axiosInstance.post("users/register/", data);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(
+//       error?.response?.data?.message || error?.message || "Something Went Wrong"
+//     );
+//   }
+// };
+
+// const loginUser = async (data) => {
+//   try {
+//     const response = await axiosInstance.post("users/login/", data);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(
+//       error?.response?.data?.message || error?.message || "Something Went Wrong"
+//     );
+//   }
+// };
+
+// const logoutUser = async () => {
+//   try {
+//     const response = await axiosInstance.get("users/logout/");
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(
+//       error?.response?.data?.message || error?.message || "Something Went Wrong"
+//     );
+//   }
+// };
+
+// export { startUser, registerUser, loginUser, logoutUser };
+
+
+
+
 import axios from "axios";
 import { VITE_APP_URL } from "@/config/config";
 
@@ -9,8 +72,6 @@ const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
-
-// ... rest of the file remains the same ...
 
 const startUser = async () => {
   try {
@@ -56,4 +117,26 @@ const logoutUser = async () => {
   }
 };
 
-export { startUser, registerUser, loginUser, logoutUser };
+const forgotPassword = async (data) => {
+  try {
+    const response = await axiosInstance.post("users/forgot-password", data);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error?.response?.data?.message || error?.message || "Password reset failed"
+    );
+  }
+};
+
+const changePassword = async (data) => {
+  try {
+    const response = await axiosInstance.post("users/change-password", data);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error?.response?.data?.message || error?.message || "Password change failed"
+    );
+  }
+};
+
+export { startUser, registerUser, loginUser, logoutUser, forgotPassword, changePassword };
