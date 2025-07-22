ed1e88c167898a2f21a218af15444c9ca19a3461
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-07-22 12:20:22 +0530

    Update user.routes.js

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,8 +1,29 @@
+// import {
+//   start,
+//   loginUser,
+//   logoutUser,
+//   registerUser,
+// } from "../controller/user.controller.js";
+// import { Router } from "express";
+// import { isUserAvailable } from "../middleware/auth.js";
+
+// const router = Router();
+
+// router.get("/", isUserAvailable, start);
+// router.post("/register", registerUser);
+// router.post("/login", loginUser);
+// router.get("/logout", isUserAvailable, logoutUser);
+
+// export default router;
+
+
 import {
   start,
   loginUser,
   logoutUser,
   registerUser,
+  forgotPassword,
+  changePassword
 } from "../controller/user.controller.js";
 import { Router } from "express";
 import { isUserAvailable } from "../middleware/auth.js";
@@ -13,5 +34,7 @@ router.get("/", isUserAvailable, start);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", isUserAvailable, logoutUser);
+router.post("/forgot-password", forgotPassword); // Public route for password reset
+router.post("/change-password", isUserAvailable, changePassword); // Protected route for password change
 
 export default router;
