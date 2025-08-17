c6fdd268dab77af0d6b8c741236de684cd295dca
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-08-18 00:33:47 +0530

    Update config.js

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -2,7 +2,7 @@ const AUTH_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const GEMENI_API_KEY = import.meta.env.VITE_GEMENI_API_KEY;
 const VITE_APP_URL = import.meta.env.VITE_APP_URL;
-const VITE_PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL; // Add this line
+const VITE_PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL;
+const VITE_GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID; // <-- ADD THIS
 
-
-export { AUTH_KEY, API_KEY, VITE_PUBLIC_URL, GEMENI_API_KEY,VITE_APP_URL };
+export { AUTH_KEY, API_KEY, VITE_PUBLIC_URL, GEMENI_API_KEY,VITE_APP_URL, VITE_GOOGLE_CLIENT_ID }; // <-- ADD VITE_GOOGLE_CLIENT_ID
